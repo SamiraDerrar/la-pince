400b2de54ea8d572189cb8431a713da4488ae994
Sam <samira.derrar@oclock.school>
2026-03-08 15:38:34 +0100

    sequelize.client pour render modifié

--- api/database/sequelize-client.js
+++ api/database/sequelize-client.js
@@ -1,20 +1,19 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,
-	{
-		define: {
-			// Une convention pour que Sequelize ne mette pas les noms de table au pluriel
-			freezeTableName: true,
-			// Ajoute les colonnes createdAt et updatedAt à chaque table
-			timestamps: true,
-			// Utilise le snake_case pour les champs auto-générés (createdAt -> created_at)
-			underscored: true,
-		},
-	}
-);
-
-// synchronize la BDD et reset la bdd
-await sequelize.sync({ force: true });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  define: {
+    freezeTableName: true,
+    timestamps: true,
+    underscored: true,
+  },
+});
 
 export default sequelize;
